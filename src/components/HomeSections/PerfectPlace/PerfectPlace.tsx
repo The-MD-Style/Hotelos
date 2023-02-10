caf0716c1322345c4f5a51c9e6d87567213a51a2
Constantin <costeapocitaru1@gmail.com>
2023-02-10 11:40:05 +0200

    feat(section): remake mobile version for header section

--- src/components/HomeSections/PerfectPlace/PerfectPlace.tsx
+++ src/components/HomeSections/PerfectPlace/PerfectPlace.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import PerfectPlaceView from "./PerfectPlace.view";
 
 type Props = {};
 
 const PerfectPlace = (props: Props) => {
-  return <PerfectPlaceView></PerfectPlaceView>;
+  const [pageIndex, setPageIndex] = useState<number>(0);
+
+  const next = () => {setPageIndex((prev) => prev===3?0:++prev)}
+  const prev = () => {setPageIndex((prev) => prev===0?3:--prev)}
+
+  return <PerfectPlaceView value={pageIndex} next={next} prev={prev}></PerfectPlaceView>;
 };
 
 export default PerfectPlace;
