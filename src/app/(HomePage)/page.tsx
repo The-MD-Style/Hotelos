4e32a3483f8a38d1547ad2d04be17906aa520e33
themdstyle <costeapocitaru1@gmail.com>
2023-12-15 15:41:34 +0200

    test

--- src/app/(HomePage)/page.tsx
+++ src/app/(HomePage)/page.tsx
@@ -6,10 +6,27 @@ import MostPopular from "@/components/HomeSections/MostPopular/MostPopular";
 import BestFacilities from "@/components/HomeSections/BestFacilities/BestFacilities";
 import History from "@/components/HomeSections/History/History";
 import Map from "@/components/HomeSections/Map/Map";
+import { useEffect, useState } from "react";
 
 // const inter = Inter({ subsets: ["latin"] });
 
+
+
+
 export default function Home() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.npms.io/v2/search?q=react');
+      const newData = await response.json();
+      setData(newData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SectionContainer className={""}>
       <PerfectPlace></PerfectPlace>
@@ -17,6 +34,11 @@ export default function Home() {
       <BestFacilities></BestFacilities>
       <History></History>
       <Map></Map>
+      <div>
+        {data}
+      </div>
     </SectionContainer>
   );
 }
+
+
